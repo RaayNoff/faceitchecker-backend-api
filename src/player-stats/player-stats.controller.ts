2ee64bf15094e7c3e20378d29f89e5a0e4d70c5f
RaayNoff <kraxtv471365@gmail.com>
2025-02-02 20:29:50 +0700

    removed unused import

--- src/player-stats/player-stats.controller.ts
+++ src/player-stats/player-stats.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { PlayerStatsService } from './player-stats.service';
 import { PlayerStatsInputDto } from './dtos/PlayerStatsInputDto';
 
